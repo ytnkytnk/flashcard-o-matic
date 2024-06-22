ca92878dcb124cb70269a5f15daf3f1a9d6746e8
Yoko Tanaka <yotanaka@indeed.com>
2024-06-22 10:39:33 +0900

    add handleLastCard for Study

--- src/contents/Study.js
+++ src/contents/Study.js
@@ -7,7 +7,7 @@ function Study() {
   const navigate = useNavigate();
 
   const [deck, setDeck] = useState("");
-  const [currentCardId, setCurrentCardId] = useState("1");
+  const [currentCardId, setCurrentCardId] = useState(1);
   const [isFlipped, setIsFlipped] = useState(false);
 
   useEffect(() => {
@@ -22,12 +22,13 @@ function Study() {
     fetchDeck();
   }, [deckId]);
 
+  // add Loading for pending case
   if (!deck) {
     return <div>Loading...</div>;
   }
 
   const cards = deck.cards.filter((card) => card.deckId === Number(deckId));
-  //   console.log(cards);
+  console.log("cards:", cards);
 
   // if there are more than 3 cards, return flashcard #1
   // if not, show 'Not enough cards' message with 'Add Cards' button
@@ -53,6 +54,21 @@ function Study() {
   }
 
   // if reach to the end of the cards, show the message
+  function handleLastCard() {
+    if (
+      window.confirm(
+        `Restart cards?\r\n\r\nClick 'cancel' to return to the home page.`
+      )
+    ) {
+      // yes clicked >> restart cards
+      setCurrentCardId(1);
+      navigate(`/decks/${deck.id}/study`);
+    } else {
+      // cancel clicked >> go to Home
+      navigate(`/`);
+    }
+  }
+
   return (
     <div>
       <h1>{deck.name}: Study</h1>
@@ -60,35 +76,36 @@ function Study() {
         <h3>
           Card {currentCardId} of {cards.length}
         </h3>
-        {!isFlipped ? (
-          <p>{cards[currentCardId - 1].front}</p>
-        ) : (
-          <p>{cards[currentCardId - 1].back}</p>
-        )}
-      </div>
-      <div className="buttons">
-        <button
-          onClick={() => {
-            setIsFlipped(!isFlipped);
-          }}
-        >
-          Flip
-        </button>
-        {!isFlipped ? null : (
+        {cards
+          .filter((card) => card.id === currentCardId)
+          .map((card) =>
+            !isFlipped ? <p>{card.front}</p> : <p>{card.back}</p>
+          )}
+        <div className="buttons">
           <button
-            style={{
-              border: "solid 1px blue",
-              color: "white",
-              "background-color": "blue",
-            }}
             onClick={() => {
-              setCurrentCardId(Number(currentCardId) + 1);
               setIsFlipped(!isFlipped);
             }}
           >
-            Next
+            Flip
           </button>
-        )}
+          {!isFlipped ? null : (
+            <button
+              style={{
+                border: "solid 1px blue",
+                color: "white",
+                backgroundColor: "blue",
+              }}
+              onClick={() => {
+                setCurrentCardId(currentCardId + 1);
+                setIsFlipped(!isFlipped);
+                if (currentCardId === deck.cards.length) handleLastCard();
+              }}
+            >
+              Next
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
