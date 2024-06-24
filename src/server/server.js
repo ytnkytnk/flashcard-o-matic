307c06ce4f209acfb1bc0baea7cd52d76cc4d06e
Yoko Tanaka <yotanaka@indeed.com>
2024-06-24 13:38:53 +0900

    update files for port 5000

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,18 @@
+// for app
+
 const { PORT = 3000 } = process.env;
 const app = require("../appServer");
 
 const listener = () => console.log(`Listening on Port ${PORT}`);
 
 app.listen(PORT, listener);
+
+// for json-server
+
+const express = require("express");
+const json = express();
+const jsonPort = 5000;
+
+json.use(express.static("/utils/api/index.js"));
+
+json.listen(jsonPort, () => console.log(`Listening on Port ${jsonPort}`));
